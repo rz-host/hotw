f457f136313ac5d01a0175d1e1daa45f2acf9087
RyanZim <opensrc@ryanzim.com>
2017-10-11 10:13:24 -0400

    publish: Switch to GH Pages for hosting
    generated from commit a20a47feec57e2626fb435681ac32ea487780e71

---
+++ js/default.js
@@ -0,0 +1,2 @@
+!function(){"use strict";document.getElementById("backTop").addEventListener("click",function(t){window.scroll(0,0),history.pushState("",document.title,window.location.pathname+window.location.search),t.preventDefault()})}();
+//# sourceMappingURL=default.js.map
